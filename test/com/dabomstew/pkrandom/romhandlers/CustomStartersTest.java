48885b2614c7541493a490bd38c6845780ae3968
Sean Beach <3373887+sbeach@users.noreply.github.com>
2025-06-29 14:31:05 -0500

    new test class

---
+++ test/com/dabomstew/pkrandom/romhandlers/CustomStartersTest.java
@@ -0,0 +1,8 @@
+package com.dabomstew.pkrandom.romhandlers;
+
+/**
+ * Tests the customStarters method in AbstractRomHandler using a mock implementation.
+ * This avoids the need for a real ROM file.
+ */
+class CustomStartersTest {
+}
