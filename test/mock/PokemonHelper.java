362bce9c28a280bebf54b7e620bbb199eb8d305c
Sean Beach <3373887+sbeach@users.noreply.github.com>
2025-06-29 19:34:35 -0500

    add helper for evos

--- test/mock/PokemonHelper.java
+++ test/mock/PokemonHelper.java
@@ -1,6 +1,11 @@
 package mock;
 
+import com.dabomstew.pkrandom.pokemon.Evolution;
+import com.dabomstew.pkrandom.pokemon.EvolutionType;
 import com.dabomstew.pkrandom.pokemon.Pokemon;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PokemonHelper {
 
@@ -9,5 +14,14 @@ public class PokemonHelper {
         poke.number = number;
         poke.name = name;
         return poke;
+    }
+
+    /**
+     * A helper to cleanly link two Pokémon in an evolution chain.
+     * This abstracts away the need to manually create and add Evolution objects.
+     */
+    public static void link(Pokemon from, Pokemon to) {
+        Evolution evolution = new Evolution(from, to, true, EvolutionType.LEVEL, 0);
+        from.evolutionsFrom.add(evolution);
     }
 }
